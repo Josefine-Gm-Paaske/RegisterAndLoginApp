0c914a411403409714b587907c61272487e1b1ea
Josefine Paaske <1086237@ucn.dk>
2021-07-08 20:31:25 +0200

    SQL so we can sign up as a new user
    Adding the new user to the database

--- RegisterAndLogin/Services/UsersDAO.cs
+++ RegisterAndLogin/Services/UsersDAO.cs
@@ -40,5 +40,34 @@ namespace RegisterAndLogin.Services
             }
             return success;
         }
+        public bool AddUser(UserModel user)
+        {
+            bool success = false;
+            string sqlStatement = "INSERT INTO dbo.Users (username, password) VALUES (@username, @password)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.Add("@username", System.Data.SqlDbType.VarChar, 40).Value = user.UserName;
+                command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 40).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    int reader = command.ExecuteNonQuery();
+
+                    if (reader > 0)
+                    {
+                        success = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return success;
+        }
     }
 }
