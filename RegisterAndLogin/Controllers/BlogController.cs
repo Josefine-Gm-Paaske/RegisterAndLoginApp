ceb9ec6549b0d9205a5f98347d7806ff95694d40
Josefine Paaske <1086237@ucn.dk>
2021-07-09 21:50:03 +0200

    Index for the blog

---
+++ RegisterAndLogin/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLogin.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
